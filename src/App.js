c755aa2e5650df863f7e6da8e351affffa29325f
Dzianis Matsiushyn <dzmats@gmail.com>
2022-08-29 19:40:50 +0200

    Routing added

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import React from 'react';
+import About from './About';
+import Home from './Home';
+
+function App() {
+    return (
+        <div className='App'>
+            <div>
+                <nav>
+                    <ul id="navigation">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
